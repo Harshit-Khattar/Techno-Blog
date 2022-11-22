48b73259f38bfd07eab4c089ee2ece02b18a2c25
Harshit Khattar <hkhatter03@gmail.com>
2022-11-22 17:29:21 +0530

    Changes for back-end

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const mongoose = require("mongoose");
 
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Hey! Welcome to The Techno Blog. \n Here I will be sharing a lot of stuff related to the field of tech. You'll be able to find everything ranging from guides on various tech stacks to my experiences during technical interviews. Do contact me if you have any suggestion or feedback. Happy Reading :) ";
+const aboutContent = "Hey! I'm Harshit Khattar, a pre-final year student pursuing B.Tech in Information Technology from Maharaja Surajmal Institute of Technology, New Delhi. Skilled at Web Development, Data Structures & Algorithms, I am a tech enthusiast who loves to explore new things & interact with people. ";
+const contactContent = "You can contact me via my E-mail: hkhatter03@gmail.com or message me on LinkedIn: linkedin.com/in/hkhattar14/ . Any feedback or suggestion is highly appreciated!";
 
 const app = express();
 
@@ -19,11 +19,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-main().catch(err => console.log(err));
+// main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/test');
-}
+// async function main() {
+//   await mongoose.connect('mongodb://localhost:27017/test');
+// }
+mongoose.connect("mongodb+srv://hkhattar14:!41rattahkH@cluster0.ez3lfq1.mongodb.net/blogDB");
 
 const postSchema = {
   title: String,
@@ -85,3 +86,4 @@ app.get("/contact", function(req, res){
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
