b5139cfe8fbd55baa56fc7234fadec1bfb287b09
Harshit Khattar <hkhatter03@gmail.com>
2022-11-23 13:21:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-main().catch(err => console.log(err));
+//main().catch(err => console.log(err));
 
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/test');
-}
-
+//async function main() {
+ // await mongoose.connect('mongodb://localhost:27017/test');
+//}
+mongoose.connect("mongodb+srv://hkhattar14:!41rattahkH@cluster0.ez3lfq1.mongodb.net/blogDB");
 const postSchema = {
   title: String,
   content: String
